b0d44b10abe856eb4eae13dd25a3d8f6427664b8
Elliot <elliotlunness@lazercube.com>
2022-09-01 19:35:03 +0100

    Add command now add builds and packages

--- pkg/cmd/cli/command/add.go
+++ pkg/cmd/cli/command/add.go
@@ -2,32 +2,64 @@ package command
 
 import (
 	"fmt"
+	"path"
 
 	"github.com/rocketblend/rocketblend/pkg/client"
+	"github.com/rocketblend/rocketblend/pkg/core/library"
 	"github.com/spf13/cobra"
 )
 
 func NewAddCommand(client *client.Client) *cobra.Command {
-	var path string
+	var dir string
 
 	c := &cobra.Command{
 		Use:   "add",
-		Short: "Adds a local build of blender to the database",
+		Short: "Adds a local build/package to the database",
 		Long:  ``,
 		Run: func(cmd *cobra.Command, args []string) {
-			if err := client.AddInstall(path); err != nil {
-				fmt.Printf("Error adding build: %v\n", err)
-				return
-			}
+			name := path.Base(dir)
 
-			fmt.Printf("Build %s added\n", path)
+			switch name {
+			case library.BuildFile:
+				err := addInstall(client, dir)
+				if err != nil {
+					fmt.Printf("Error adding build: %v\n", err)
+				}
+			case library.PackgeFile:
+				err := addAddon(client, dir)
+				if err != nil {
+					fmt.Printf("Error adding build: %v\n", err)
+				}
+			default:
+				fmt.Printf("Unknown file type: %s\n", name)
+			}
 		},
 	}
 
-	c.Flags().StringVarP(&path, "path", "p", "", "path to the version to add")
+	c.Flags().StringVarP(&dir, "path", "p", "", "path to the build/package to add")
 	if err := c.MarkFlagRequired("path"); err != nil {
 		fmt.Println(err)
 	}
 
 	return c
+}
+
+func addInstall(client *client.Client, path string) error {
+	if err := client.AddInstall(path); err != nil {
+		return fmt.Errorf("error adding build: %v", err)
+	}
+
+	fmt.Printf("Build %s added\n", path)
+
+	return nil
+}
+
+func addAddon(client *client.Client, path string) error {
+	if err := client.AddAddon(path); err != nil {
+		return fmt.Errorf("error adding package: %v", err)
+	}
+
+	fmt.Printf("Package %s added\n", path)
+
+	return nil
 }
