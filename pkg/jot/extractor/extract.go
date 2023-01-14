28f95924cc22b9c3bcb261b85ea3adf7dbfd0dab
Elliot <git@lazercube.com>
2023-01-14 18:22:27 +0000

    more fixes

--- pkg/jot/extractor/extract.go
+++ pkg/jot/extractor/extract.go
@@ -9,23 +9,23 @@ import (
 func extractDMG(filePath string, destination string, imageName string, contentName string) error {
 	// Use hdiutil command to mount the .dmg file
 	cmd := exec.Command("hdiutil", "attach", filePath)
-	_, err := cmd.Output()
+	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("failed to mount .dmg file")
 	}
 
-	volumePath := filepath.Join("Volumes", imageName)
+	volumePath := filepath.Join("/Volumes", imageName)
 
 	// Use cp command to copy the contents of the mounted .dmg to the destination
 	cmd = exec.Command("cp", "-R", filepath.Join(volumePath, contentName), destination)
-	_, err = cmd.Output()
+	err = cmd.Run()
 	if err != nil {
-		return fmt.Errorf("failed to copy contents of .dmg file: %w", err)
+		return fmt.Errorf("failed to copy contents of .dmg file")
 	}
 
 	// Use hdiutil command to detach the mounted .dmg
 	cmd = exec.Command("hdiutil", "detach", volumePath)
-	_, err = cmd.Output()
+	err = cmd.Run()
 	if err != nil {
 		return err
 	}
