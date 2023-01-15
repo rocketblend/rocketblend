d0f666f6ba24933701beeb6e100418744e8fdc96
Elliot <git@lazercube.com>
2023-01-15 21:25:03 +0000

    Removed old client code

--- pkg/client/resource.go
+++
@@ -1,8 +0,0 @@
-package client
-
-import "github.com/rocketblend/rocketblend/pkg/core/resource"
-
-func (c *Client) FindResource(key string) (*resource.Resource, error) {
-	// TODO: save out if not found.
-	return c.resource.FindByName(key)
-}
