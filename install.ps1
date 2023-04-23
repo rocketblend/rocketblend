fbb64f9d915851f1446ba307187ea375cf606d7b
Elliot <git@lazercube.com>
2023-04-23 01:03:12 +0100

    Fixed powershell script

--- install.ps1
+++ install.ps1
@@ -1,30 +1,50 @@
-$Repo = "https://github.com/rocketblend/rocketblend"
-$AppName = "rktb"
-$ReleasesApi = "https://api.github.com/repos/$Repo/releases/latest"
+$ErrorActionPreference = 'Stop'
 
-# Determine the platform and architecture
-$OS = "windows"
-$Arch = "amd64" # Currently assuming amd64, modify if needed for other architectures
+$ExecutableName = "rktb"
 
-# Download the appropriate binary for the platform and architecture
-$DownloadUrl = (Invoke-WebRequest -Uri $ReleasesApi | ConvertFrom-Json | `
-    Where-Object { $_.browser_download_url -match "${OS}-${Arch}" }).browser_download_url
-$Destination = Join-Path -Path $HOME -ChildPath ".local\bin\$AppName.exe"
+$APIUrl = "https://api.github.com/repos/rocketblend/rocketblend/releases/latest"
 
-if (-Not (Test-Path -Path (Split-Path -Path $Destination -Parent))) {
-    New-Item -ItemType Directory -Path (Split-Path -Path $Destination -Parent) | Out-Null
+# Fetch the latest release info
+$LatestRelease = Invoke-RestMethod -Uri $APIUrl
+
+# Determine the OS and Architecture
+$OS = if ([Environment]::Is64BitOperatingSystem) { "Windows_x86_64" } else { "Windows_i386" }
+
+# Find the desired asset based on the OS and Architecture
+$DownloadUrl = ($LatestRelease.assets | Where-Object { $_.browser_download_url -like "*$ExecutableName_$OS.zip" }).browser_download_url
+
+if (!$DownloadUrl) {
+    throw "Failed to find the download URL for the $OS version of $ExecutableName"
 }
 
-Write-Host "Downloading $AppName for $OS-$Arch..."
-Invoke-WebRequest -Uri $DownloadUrl -OutFile $Destination
+# Download and extract the ZIP file
+$OutputPath = "$Env:LOCALAPPDATA/Programs/RocketBlend/"
+$ZipFile = "$Env:TEMP/$ExecutableName.zip"
 
-# Add the destination directory to the user's PATH, if not already present
-$PathKey = [Environment]::GetEnvironmentVariable("Path", "User")
-$DestinationDir = Split-Path -Path $Destination -Parent
+Invoke-WebRequest -Uri $DownloadUrl -OutFile $ZipFile
+Expand-Archive -Path $ZipFile -DestinationPath $Env:TEMP -Force
 
-if (-Not ($PathKey -split ";" -contains $DestinationDir)) {
-    Write-Host "Adding $AppName to the PATH..."
-    [Environment]::SetEnvironmentVariable("Path", $PathKey + ";" + $DestinationDir, "User")
+# Move the content of the extracted folder to the OutputPath
+$ExtractedFolderPath = "$Env:TEMP/${ExecutableName}_${OS}"
+
+# Ensure the OutputPath directory exists
+if (-not (Test-Path -Path $OutputPath -PathType Container)) {
+    New-Item -Path $OutputPath -ItemType Directory | Out-Null
 }
 
-Write-Host "Installation complete. $AppName is now available in $Destination"
+Move-Item -Path "$ExtractedFolderPath/*" -Destination $OutputPath -Force
+
+# Cleanup
+Remove-Item $ZipFile
+Remove-Item $ExtractedFolderPath -Recurse
+
+# Add the installation path to the PATH environment variable
+$UserPath = [Environment]::GetEnvironmentVariable("Path", "User")
+$PathArray = $UserPath -split ";"
+if (-not ($PathArray -contains $OutputPath)) {
+    $NewUserPath = $UserPath + ";" + $OutputPath
+    [Environment]::SetEnvironmentVariable("Path", $NewUserPath, "User")
+}
+
+Write-Host "$ExecutableName has been installed successfully!"
+Write-Host "Please restart your PowerShell session for the changes to take effect."
