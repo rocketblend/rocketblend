85bf2eb2da7c21d2bd0dccd6ce4a7f3828b7f7a1
Elliot <git@lazercube.com>
2023-04-23 01:10:30 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,41 @@
-#!/bin/bash
-
+#!/bin/sh
 set -e
 
-REPO="https://github.com/rocketblend/rocketblend"
-APP_NAME="rktb"
-RELEASES_API="https://api.github.com/repos/$REPO/releases/latest"
+EXECUTABLE_NAME="rktb"
 
-# Determine the platform and architecture
-OS="$(uname | tr '[:upper:]' '[:lower:]')"
+API_URL="https://api.github.com/repos/rocketblend/rocketblend/releases/latest"
+
+# Fetch the latest release info
+LATEST_RELEASE=$(curl -sL "$API_URL")
+
+# Determine the OS and Architecture
+OS="$(uname -s)"
 ARCH="$(uname -m)"
-case $ARCH in
-    x86_64)
-        ARCH="amd64"
-        ;;
-    arm*)
-        ARCH="arm"
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1
-        ;;
+case "$OS" in
+  Darwin*)  OS="Darwin" ;;
+  Linux*)   OS="Linux" ;;
+  *)        printf "Your OS is not supported by this script\n"; exit 1 ;;
+esac
+case "$ARCH" in
+  x86_64) ARCH="x86_64" ;;
+  *)      printf "Your architecture is not supported by this script\n"; exit 1 ;;
 esac
 
-# Download the appropriate binary for the platform and architecture
-DOWNLOAD_URL="$(curl -s $RELEASES_API | grep "browser_download_url.*${OS}-${ARCH}" | cut -d : -f 2,3 | tr -d \" | tr -d ' ')"
-DESTINATION="/usr/local/bin/$APP_NAME"
+# Find the desired asset based on the OS and Architecture
+DOWNLOAD_URL=$(echo "$LATEST_RELEASE" | grep -o "https://.*${EXECUTABLE_NAME}_${OS}_${ARCH}\.tar\.gz")
 
-echo "Downloading $APP_NAME for $OS-$ARCH..."
-curl -L -o "$DESTINATION" "$DOWNLOAD_URL"
+if [ -z "$DOWNLOAD_URL" ]; then
+  printf "Failed to find the download URL for the %s version of %s\n" "$OS" "$EXECUTABLE_NAME"
+  exit 1
+fi
 
-# Set the executable permissions
-chmod +x "$DESTINATION"
+# Download and extract the tarball
+TEMP_DIR=$(mktemp -d)
+curl -sL "$DOWNLOAD_URL" | tar xz -C "$TEMP_DIR"
 
-echo "Installation complete. $APP_NAME is now available in $DESTINATION"
+# Move the binary to a directory in PATH
+INSTALL_PATH="/usr/local/bin"
+sudo mv "$TEMP_DIR/$EXECUTABLE_NAME" "$INSTALL_PATH"
+rm -rf "$TEMP_DIR"
+
+printf "%s has been installed successfully!\n" "$EXECUTABLE_NAME"
