3cbbe102ba1c41deb49263dbaf91b6bed3681460
Elliot <git@lazercube.com>
2023-05-17 18:06:54 +0100

    Updated cli code

--- cmd/rocketblend/main.go
+++ cmd/rocketblend/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/rocketblend/rocketblend/pkg/cli"
 )
 
 func main() {
-	if err := cli.Execute(); err != nil {
+	app, err := cli.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := app.Execute(); err != nil {
 		return
 	}
 }
